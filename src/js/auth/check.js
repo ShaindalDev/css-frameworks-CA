29cea3ce81aa7c51186ddbacd08fb129446aabcd
ShaindalDev <89133440+ShaindalDev@users.noreply.github.com>
2022-10-04 12:51:33 +0200

    added check function to see if you are logged in

--- src/js/auth/check.js
+++ src/js/auth/check.js
@@ -1,5 +1,5 @@
 /**
- * Function to check if you are allready logged in or not. 
+ * Function to check if you are already logged in or not. 
  * If logged in and Auth token is valid, you are redirected to your profile page, 
  * If not logged and Auth token is invalid, you are redirected to login screen.
  * When check see you are logged in and Auth token is valid, login button is replaced with logout button.
