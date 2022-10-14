c0befcbfb5f9539c091bb74b817198bf233865a3
ShaindalDev <89133440+ShaindalDev@users.noreply.github.com>
2022-10-14 22:47:10 +0200

    Playing around with a sorting by date

--- src/js/components/filterFunction.mjs
+++ src/js/components/filterFunction.mjs
@@ -1,21 +1,36 @@
-import { API_SOCIAL_URL } from "../api/constants.mjs";
+const todayPostSelection = document.querySelector('#today');
+const oldestPostSelection = document.querySelector('#oldest');
+const lastWeekSelection = document.querySelector('#lastWeek');
+const removeFilterSelection = document.querySelector('#clear')
 
-const filterPostContainer = document.querySelector('.index-post-box');
 
-const searchPictures = () => {
-    postContainer.textContent = "";
-    fetchCall(postswithac, getWithJwt).then((data) => {
-      const filteredPictures = data.filter((element) => {
-        return element.media.substring(5, 0) === "https";
-      });
-      filteredPictures.forEach((element) => {
-        htmlToRender(postContainer, element);
-      });
-      postContainer.insertAdjacentHTML(
-        "afterbegin",
-        "<p>Results " + filteredPictures.length + " posts with pictures</p>"
-      );
-      //   commentList();
-      //   likeClick();
-    });
-  };
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
+const week = day * 7;
+const fortnight = day * 21;
+
+const today = new Date();
+
+todayPostSelection.addEventListener("change", () => {
+  filterDate(today);
+});
+
+oldestPostSelection.addEventListener("change", () => {
+  const threeweeks = new Date(today - fortnight);
+  filterDate(threeweeks);
+});
+
+lastWeekSelection.addEventListener("change", () => {
+  const lastWeek = new Date(today - week);
+  filterDate(lastWeek);
+});
+
+removeFilterSelection.addEventListener("change", () => {
+  
+})
+
+function filterDate(searchDate) {
+  const filteredPostsByDate = 
+}
