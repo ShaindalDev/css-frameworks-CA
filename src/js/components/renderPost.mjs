57f8e6153f373dc0e7bd62783a290b467927693c
ShaindalDev <89133440+ShaindalDev@users.noreply.github.com>
2022-10-16 08:46:36 +0200

    added some description and cleaned up code

--- src/js/components/renderPost.mjs
+++ src/js/components/renderPost.mjs
@@ -1,3 +1,7 @@
+/**
+ * This renders the HTML for all posts. 
+ * @param {*} postsToRender 
+ */
 export function renderPosts(postsToRender) {
     const postContainer = document.querySelector('#postContainer');
 
@@ -39,61 +43,3 @@ export function renderPosts(postsToRender) {
     });
     
 };
-
-  /**
- * Filters and Displays posts based on three filtering options: 
- * todays posts - yesterdays posts - posts with media
- * @param {string} postDataList
- * @param {string} parent 
- */
-
-   export function renderFilteredPost(postDatalist, parent) {
-    const container = document.querySelector('#postContainer');
-    const filterTwentyFourHours = document.querySelector('#yesterdayPost');
-    const filterTwoDays = document.querySelector('#twoDaysAgo');
-    const filterTwoWeeks = document.querySelector('#twoWeeks');
-  
-    const day = 1000 * 60 * 60 * 24;
-    const twoDays = day * 2;
-    const twoWeeks = day * 14;
-    const currentTime = new Date();
-  
-    filterTwentyFourHours.addEventListener("click", () => {
-      const twentyFour = new Date(currentTime - day).toISOString();
-      container.innerHTML = "";
-  
-      const filteredDates = postDatalist.filter(post => post.updated <= twentyFour)
-  
-      filteredDates.forEach(i => {
-        if (i) {
-          parent.append(renderPosts(i)) 
-        }
-      }) 
-    })
-  
-    filterTwoDays.addEventListener("click", () => {
-      const twoDaysAgo = new Date(currentTime - twoDays).toISOString();
-      container.innerHTML = "";
-  
-      const filteredDates = postDatalist.filter(post => post.updated <= twoDaysAgo)
-  
-      filteredDates.forEach(i => {
-        if (i) {
-          parent.append(renderPosts(i)) 
-        }
-      }) 
-    })
-
-    filterTwoWeeks.addEventListener("click", () => {
-      const twoWeeks = new Date(currentTime - twoWeeks).toISOString();
-      container.innerHTML = "";
-  
-      const filteredDates = postDatalist.filter(post => post.updated <= twoWeeks)
-  
-      filteredDates.forEach(i => {
-        if (i) {
-          parent.append(renderPosts(i)) 
-        }
-      }) 
-    })
-  }
